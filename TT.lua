cc48622b81028dbbe91cf9b65bdf0ba1ce1c8e59
nphkelakxngsungnein888-commits <nphkelakxngsungnein888@gmail.com>
2026-03-23 22:03:08 +0700

    Update TT.lua

--- TT.lua
+++ TT.lua
@@ -103,3 +103,43 @@ end)
 closeBtn.MouseButton1Click:Connect(function()
 	gui:Destroy()
 end)
+-- Drag System
+local UserInputService = game:GetService("UserInputService")
+
+local dragging = false
+local dragInput
+local dragStart
+local startPos
+
+frame.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		dragging = true
+		dragStart = input.Position
+		startPos = frame.Position
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+frame.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		local delta = input.Position - dragStart
+		
+		frame.Position = UDim2.new(
+			startPos.X.Scale,
+			startPos.X.Offset + delta.X,
+			startPos.Y.Scale,
+			startPos.Y.Offset + delta.Y
+		)
+	end
+end)
